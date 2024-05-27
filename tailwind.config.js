a1bb44c8c5f963dfb956a0aff7fd077cac397ae0
Wijaya <wijayajay151@gmai.com>
2024-05-27 19:29:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#444CF7',
+        dark:'#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
+
